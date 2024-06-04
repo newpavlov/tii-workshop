b3f990b5e2d94d1ffebcbca4e103a86a335f647e
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-04 15:20:26 +0400

    Implement p12

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn celsius2farenheit(celsius: i32) -> i32 {
+    return (celsius * (9 / 5)) + 32;
+}
+
+fn farenheit2celsius(farenheit: i32) -> i32 {
+    return (farenheit - 32) / (9 / 5);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(farenheit2celsius(celsius2farenheit(2)), 2);
+    }
+}
