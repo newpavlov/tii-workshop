e24301c0a34aac98062475376b03c6e0b9316a26
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-05 19:16:16 +0400

    defect: bad implementation of fahrenheit to celsius (and viceversa).
    detected by clippy: https://rust-lang.github.io/rust-clippy/master/index.html\#/identity_op

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -3,11 +3,13 @@ fn main() {
 }
 
 fn celsius2farenheit(celsius: i32) -> i32 {
-    return (celsius * (9 / 5)) + 32;
+    // Careful with https://rust-lang.github.io/rust-clippy/master/index.html#/identity_op
+    return ((celsius * 9) / 5) + 32;
 }
 
 fn farenheit2celsius(farenheit: i32) -> i32 {
-    return (farenheit - 32) / (9 / 5);
+    // Careful with https://rust-lang.github.io/rust-clippy/master/index.html#/identity_op
+    return (5 * (farenheit - 32)) / 9;
 }
 
 #[cfg(test)]
@@ -16,11 +18,49 @@ mod tests {
 
     #[test]
     fn loop_from_celsius() {
-        assert_eq!(farenheit2celsius(celsius2farenheit(2)), 2);
+        let same_celsius_value = -5;
+        assert_eq!(
+            farenheit2celsius(celsius2farenheit(same_celsius_value)),
+            same_celsius_value
+        );
     }
 
     #[test]
     fn loop_from_farenheit() {
-        assert_eq!(celsius2farenheit(farenheit2celsius(2)), 2);
+        let same_fahrenheit_value = 212;
+        assert_eq!(
+            celsius2farenheit(farenheit2celsius(same_fahrenheit_value)),
+            same_fahrenheit_value
+        );
+    }
+
+    #[test]
+    fn kat() {
+        // Source: https://fr.farnell.com/en-FR/convertisseur-temperature
+        let equivalences_fahrenheit_and_celsius = [(23, -5), (194, 90), (212, 100), (392, 200)];
+        for equivalence in equivalences_fahrenheit_and_celsius {
+            let actual_celsius = farenheit2celsius(equivalence.0);
+            assert_eq!(
+                equivalence.1, actual_celsius,
+                "error: fahrenheit {} is not celsius {}. actual: {}",
+                equivalence.0, equivalence.1, actual_celsius
+            );
+
+            let actual_fahrenheit = celsius2farenheit(equivalence.1);
+            assert_eq!(
+                equivalence.1, actual_celsius,
+                "error: celsius {} is not fahrenheit {}. actual: {}",
+                equivalence.1, equivalence.0, actual_fahrenheit
+            );
+
+            assert_eq!(
+                celsius2farenheit(farenheit2celsius(equivalence.0)),
+                equivalence.0
+            );
+            assert_eq!(
+                farenheit2celsius(celsius2farenheit(equivalence.1)),
+                equivalence.1
+            );
+        }
     }
 }
