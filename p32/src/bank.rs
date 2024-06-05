ec4d148aeb406177b2c07d6bc0434b9bb72924e3
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-05 14:34:34 +0400

    transfer funds: happy path

--- p32/src/bank.rs
+++ p32/src/bank.rs
@@ -1,3 +1,5 @@
+use std::ptr::null;
+
 #[derive(Debug, PartialEq)]
 pub struct User {
     name: String,
@@ -56,6 +58,60 @@ impl Bank {
     pub fn new(users: Vec<User>, name: String, credit_interest: u64, debit_interest: u64) -> Self {
         Self { users, name, credit_interest, debit_interest }
     }
+    pub(crate) fn get_user_by_id(&self, user_id: String) -> Option<&User> {
+        for (index, user) in self.users.iter().enumerate() {
+            if user.name == user_id {
+                return Some(&user);
+            }
+        }
+        return None;
+    }
+    pub(crate) fn transfer(&mut self, origin_username: &str, destination_username: &str, amount: i64) -> bool {
+        let mut first_user_index = None;
+        let mut second_user_index = None;
+        // let users_iter_mut = self.users.iter_mut();
+        // let both_users_exist = users_iter_mut.filter(|user| user.name == origin_username || user.name == destination_username).count() == 2;
+        for (index, user) in self.users.iter().enumerate() {
+            if user.name == origin_username {
+                first_user_index = Some(index);
+            }
+            if user.name == destination_username {
+                second_user_index = Some(index);
+            }
+            if first_user_index.is_some() && second_user_index.is_some() {
+                break;
+            }
+        }
+        assert!(first_user_index.is_some() && second_user_index.is_some());
+
+        // let both_users_exist = users_iter_mut.filter(|user| user.name == origin_username || user.name == destination_username).count() == 2;
+        // first_user_exists = users_iter_mut(|user| user.name == origin_username).count() == 1;
+        // let users_iter_mut = self.users.iter_mut();
+        // let second_user_exists = users_iter_mut.filter(|user| user.name == destination_username).count() == 1;
+
+        // let mut first_user = self.users[first_user_exists_index as usize];
+        // first_user.set_balance(first_user.get_balance() - amount);
+        // self.users[second_user_exists_index as usize].set_balance(self.users[second_user_exists_index as usize].get_balance() + amount);
+
+        let (first_index, second_index) = match (first_user_index, second_user_index) {
+            (Some(first_index), Some(second_index)) => { (first_index, second_index) }
+            _ => todo!(),
+        };
+
+        {
+            let new_balance = self.users[first_index].get_balance() - amount;
+            self.users[first_index].set_balance(new_balance);
+        }
+        {
+            let new_balance_2 = self.users[second_index].get_balance() + amount;
+            self.users[second_index].set_balance(new_balance_2);
+        }
+
+        // self.users[first_user_index.unwrap()]
+        // second_user.map(|mut user| user.set_balance(user.get_balance()));
+
+        true
+    }
 }
 
 #[cfg(test)]
@@ -107,12 +163,25 @@ mod tests {
 
     #[test]
     fn calculate_balance_of_the_bank() {
-        let user1 = User::new("John".to_string(), 100, 1);
+        let user1 = User::new("John1".to_string(), 100, 1);
         let user2 = User::new("John".to_string(), 1, 90);
         let bank = Bank::new(vec![user1, user2], "First Bank".to_string(), 1, 4);
 
         let total_balance = bank.balance();
 
         assert_eq!(total_balance, 1 + 90);
+    }
+
+    #[test]
+    fn transfer_funds_happy_path() {
+        let user1 = User::new("user1".to_string(), 100, 1);
+        let user2 = User::new("user2".to_string(), 1, 90);
+        let mut bank = Bank::new(vec![user1, user2], "First Bank".to_string(), 1, 4);
+
+        let result = bank.transfer("user1", "user2", 2);
+
+        assert_eq!(result, true);
+        assert_eq!(bank.get_user_by_id("user1".to_string()).unwrap().balance(), -1);
+        assert_eq!(bank.get_user_by_id("user2".to_string()).unwrap().balance(), 92);
     }
 }
