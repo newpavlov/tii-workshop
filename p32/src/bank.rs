e7ac441efbe04f71440f5d2ee277a43cc976dc57
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-05 12:04:58 +0400

    create impl Bank

--- p32/src/bank.rs
+++ p32/src/bank.rs
@@ -17,6 +17,12 @@ pub struct Bank {
     debit_interest: u64,
 }
 
+impl Bank {
+    pub fn new(users: Vec<User>, name: String, credit_interest: u64, debit_interest: u64) -> Self {
+        Self { users, name, credit_interest, debit_interest }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +34,17 @@ mod tests {
         assert_eq!(user.name, "John".to_string());
         assert_eq!(user.credit_line, 1);
         assert_eq!(user.balance, 1);
+    }
+
+    #[test]
+    fn test_create_a_bank() {
+        let user1 = User::new("John".to_string(), 1, 1);
+        let user = Bank::new(vec![user1], "First Bank".to_string(), 1, 4);
+
+
+        assert_eq!(user.users.len(), 1);
+        assert_eq!(user.name, "First Bank".to_string());
+        assert_eq!(user.credit_interest, 1);
+        assert_eq!(user.debit_interest, 4);
     }
 }
