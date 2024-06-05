816d1568c3784dda8b728d85ea02a47650e2ce0d
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-06-05 12:07:41 +0400

    equality for bank

--- p32/src/bank.rs
+++ p32/src/bank.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct User {
     name: String,
     credit_line: u64,
@@ -10,6 +11,7 @@ impl User {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Bank {
     users: Vec<User>,
     name: String,
@@ -47,5 +49,15 @@ mod tests {
         assert_eq!(bank.name, "First Bank".to_string());
         assert_eq!(bank.credit_interest, 1);
         assert_eq!(bank.debit_interest, 4);
+    }
+
+    #[test]
+    fn test_equality_for_bank() {
+        let user1 = User::new("John".to_string(), 1, 1);
+        let user2 = User::new("John".to_string(), 1, 1);
+        let bank = Bank::new(vec![user1], "First Bank".to_string(), 1, 4);
+        let bank2 = Bank::new(vec![user2], "First Bank".to_string(), 1, 4);
+
+        assert_eq!(bank, bank2);
     }
 }
